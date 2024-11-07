ec3f2041af485278db27807148ebeb155bafb983
jingting03 <114464535+jingting03@users.noreply.github.com>
2024-11-07 12:56:19 +0800

    Create Problem 4.中英字典.py

---
+++ Problem 4.中英字典.py
@@ -0,0 +1,16 @@
+dictionary = {
+    "dog": "狗",
+    "cat": "貓",
+    "duck": "鴨",
+    "cow": "牛",
+    "fox": "狐",
+    "狗": "dog",
+    "貓": "cat",
+    "鴨": "duck",
+    "牛": "cow",
+    "狐": "fox"
+}
+
+word = input()
+
+print(dictionary[word])
