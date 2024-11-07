a7eb99a6f47179eeb1f35677cfec2d8e6b66e5a4
jingting03 <114464535+jingting03@users.noreply.github.com>
2024-11-07 12:57:33 +0800

    Create Problem 7.星號繪三角形.py

---
+++ Problem 7.星號繪三角形.py
@@ -0,0 +1,22 @@
+shape_type = int(input())
+
+if shape_type == 1:  # 空心三角形
+    print("    *")
+    print("   * *")
+    print("  *   *")
+    print(" *     *")
+    print("*********")
+
+elif shape_type == 2:  # 實心三角形
+    print("    *")
+    print("   ***")
+    print("  *****")
+    print(" *******")
+    print("*********")
+
+elif shape_type == 3:  # 實心倒三角形
+    print("*********")
+    print(" *******")
+    print("  *****")
+    print("   ***")
+    print("    *")
